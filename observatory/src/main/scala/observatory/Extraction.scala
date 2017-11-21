52b7d93853e3f2a164b44c8c1c2701e8d5a5026b
Atsuya Kumano <at.kumano@gmail.com>
2017-11-21 15:54:58 -0800

    observatory: milestone 1 complete

--- observatory/src/main/scala/observatory/Extraction.scala
+++ observatory/src/main/scala/observatory/Extraction.scala
@@ -2,8 +2,11 @@ package observatory
 
 import java.time.LocalDate
 
+import org.apache.log4j.{Level, Logger}
+
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
 
 /**
  * 1st milestone: data extraction
@@ -11,6 +14,7 @@ import org.apache.spark.sql.types._
 object Extraction {
   import org.apache.spark.sql.SparkSession
   import org.apache.spark.sql.functions._
+  Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
 
   val spark = SparkSession
     .builder()
@@ -28,6 +32,11 @@ object Extraction {
    */
 
   def locateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
+    locateTemperaturesDS(year, stationsFile, temperaturesFile)
+  }
+
+  // Spark Dataset solution
+  def locateTemperaturesDS(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
 
     val stationsRaw = spark.read.csv(getClass.getResource(stationsFile).getFile)
       .toDF(Seq("STN", "WBAN", "Lat", "Lon"): _*).na.drop(Seq("Lat", "Lon"))
@@ -57,12 +66,16 @@ object Extraction {
       LocalDate.of(year, month.toInt, day.toInt)
     
     val temperaturesRaw = spark.read.csv(getClass.getResource(temperaturesFile).getFile)
+    def toCelcius(fahrenheit: Temperature): Temperature = 5*(fahrenheit - 32)/9
     val temperatures: Dataset[TemperaturesRow] = temperaturesRaw.map { row =>
       TemperaturesRow(row.getString(0), row.getString(1), 
-        year, row.getString(2).toInt, row.getString(3).toInt, row.getString(4).toDouble)
+        year, row.getString(2).toInt, row.getString(3).toInt, toCelcius(row.getString(4).toDouble))
     }
 
-    val joined = stations.join(temperatures, Seq("stn", "wban")).as[JoinedRow].collect
+    val joined = stations.join(temperatures,
+      stations("stn")<=>temperatures("stn") && stations("wban")<=>temperatures("wban"))
+        .drop(stations("stn")).drop(stations("wban"))
+        .as[JoinedRow].collect
 
     joined.map {case JoinedRow(_, _, location, year, month, day, temp) =>
       (LocalDate.of(year, month, day), location, temp)
@@ -75,13 +88,29 @@ object Extraction {
    * @return A sequence containing, for each location, the average temperature over the year.
    */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
-    ???
+    val tempsGrouped = records.groupBy(_._2).mapValues(_.map(_._3))
+    tempsGrouped.mapValues(ls => ls.sum/ls.size)
+  }
+
+  def locationYearlyAverageRecordsDS(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
+
+    val rows: Iterable[RecordRow] = records.map {case (_, location, temp) =>
+      RecordRow(location, temp)
+    }
+    val recordDS: Dataset[RecordRow] = rows.toList.toDS
+    val averaged = recordDS.groupBy($"loc").avg("temp")
+    val result = averaged.map {case Row(loc: Location, avgTemp: Double) =>
+      (loc, avgTemp)
+    }
+    result.collect
   }
 
 }
 
 case class StationsRow( stn: String, wban: String, loc: Location )
 case class TemperaturesRow( stn: String, wban: String,
-  year: Int, month: Int, day: Int, temp: Double )
+  year: Int, month: Int, day: Int, temp: Temperature )
 case class JoinedRow( stn: String, wban: String, loc: Location,
-  year: Int, month: Int, day: Int, temp: Double )
+  year: Int, month: Int, day: Int, temp: Temperature )
+case class RecordRow(loc: Location, temp: Temperature )
+
--- observatory/src/test/scala/observatory/ExtractionTest.scala
+++ observatory/src/test/scala/observatory/ExtractionTest.scala
@@ -4,7 +4,15 @@ import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
+import java.time.LocalDate
+
 trait ExtractionTest extends FunSuite {
 
+  test("temperatures with one entry"){
+    val oneLine = Extraction.locateTemperatures(2004, "/stations.csv", "/2004-one-entry.csv")
+    Extraction.spark.stop
+    val rounded = oneLine.map {case (date, loc, temp) => (date, loc, scala.math.round(temp*100).toDouble/100)}
+    assert(rounded === Seq( (LocalDate.of(2004, 1, 1), Location(70.933,-8.667), -5.83) ))
+  }
   
 }
